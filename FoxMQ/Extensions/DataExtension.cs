74bd6741e6cd820cc9f3915b395867db64811672
Flavio <flaviorl.net@hotmail.com>
2022-04-17 23:18:00 -0300

    DataExtension simplification

--- FoxMQ/Extensions/DataExtension.cs
+++ FoxMQ/Extensions/DataExtension.cs
@@ -6,73 +6,30 @@ namespace FoxMQ.Extensions
 {
     public static class DataExtension
     {
-        public static async Task<int> NonQueryAsync(this SqlConnection connection, string sql, SqlParameter[] parameters)
+        public static SqlCommand GetCommand(SqlConnection connection, string sql, SqlParameter[] parameters)
         {
             using SqlCommand command = new SqlCommand(sql, connection);
-
             command.Parameters.Clear();
             command.Parameters.AddRange(parameters);
 
             connection.Open();
-            return await command.ExecuteNonQueryAsync();
+            return command;
         }
 
-        public static int NonQuery(this SqlConnection connection, string sql, SqlParameter[] parameters)
-        {
-            using SqlCommand command = new SqlCommand(sql, connection);
+        public static async Task<int> NonQueryAsync(this SqlConnection connection, string sql, SqlParameter[] parameters) => 
+            await GetCommand(connection, sql, parameters)
+            .ExecuteNonQueryAsync();
 
-            command.Parameters.Clear();
-            command.Parameters.AddRange(parameters);
+        public static int NonQuery(this SqlConnection connection, string sql, SqlParameter[] parameters) => 
+            GetCommand(connection, sql, parameters)
+            .ExecuteNonQuery();
 
-            connection.Open();
-            return command.ExecuteNonQuery();
-        }
-
-        public static SqlDataReader Query(this SqlConnection connection, string sql, SqlParameter[] parameters)
-        {
-            using SqlCommand command = new SqlCommand(sql, connection);
-
-            command.Parameters.Clear();
-            command.Parameters.AddRange(parameters);
-
-            connection.Open();
-            return command.ExecuteReader();
-        }
-
-        public async static Task<SqlDataReader> QueryAsync(this SqlConnection connection, string sql, SqlParameter[] parameters)
-        {
-            using SqlCommand command = new SqlCommand(sql, connection);
-
-            command.Parameters.Clear();
-            command.Parameters.AddRange(parameters);
-
-            connection.Open();
-            return await command.ExecuteReaderAsync();
-        }
-
-        public static MessageQueue QueryMessageQueue(this SqlConnection connection, string sql, SqlParameter[] parameters)
-        {
-            using SqlCommand command = new SqlCommand(sql, connection);
-
-            command.Parameters.Clear();
-            command.Parameters.AddRange(parameters);
-
-            connection.Open();
-            SqlDataReader dataReader = command.ExecuteReader();
-
-            if (dataReader.HasRows && dataReader.Read() && !dataReader.IsDBNull(0))
-            {
-                return new MessageQueue()
-                {
-                    Id = dataReader.GetInt64(0),
-                    InsertionDate = dataReader.GetDateTime(1),
-                    Lock = dataReader.GetBoolean(2),
-                    QueueName = dataReader.GetString(3),
-                    Body = dataReader.GetString(4),
-                };
-            }
-
-            return new MessageQueue();
-        }
+        public static SqlDataReader Query(this SqlConnection connection, string sql, SqlParameter[] parameters) => 
+            GetCommand(connection, sql, parameters)
+            .ExecuteReader();
+        
+        public async static Task<SqlDataReader> QueryAsync(this SqlConnection connection, string sql, SqlParameter[] parameters) => 
+            await GetCommand(connection, sql, parameters)
+            .ExecuteReaderAsync();
     }
 }
